f853cc966241281ef19ade871961d52ca24ad979
Julie <sindhu_89@msn.com>
2017-05-22 12:18:20 +0200

    Stuff

--- Patriotisk/Patriotisk/Controllers/HomeController.cs
+++ Patriotisk/Patriotisk/Controllers/HomeController.cs
@@ -12,8 +12,7 @@ namespace Patriotisk.Controllers
     {
         public IActionResult Index()
         {
-            var client = new MongoClient("mongodb://juli301x:<eal0309892%40@>@cluster0-shard-00-00-nvnir.mongodb.net:27017,cluster0-shard-00-01-nvnir.mongodb.net:27017,cluster0-shard-00-02-nvnir.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
-            var database = client.GetDatabase("test");
+          
             
             return View();
         }
