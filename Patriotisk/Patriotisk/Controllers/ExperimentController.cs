d9bba854a71b59b021fe05e1f64ef9c02a9b6b11
Jakob Jespersen <jespersen92@gmail.com>
2017-06-02 15:01:21 +0200

    Moving forsøgs stuff to experimentcontroller

--- Patriotisk/Patriotisk/Controllers/ExperimentController.cs
+++ Patriotisk/Patriotisk/Controllers/ExperimentController.cs
@@ -5,54 +5,133 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB;
 using MongoDB.Driver;
+using Patriotisk.Models;
+using MongoDB.Bson;
+using Patriotisk.Persistence;
+using Microsoft.AspNetCore.Http;
 
 namespace Patriotisk.Controllers
 {
     public class ExperimentController : Controller
     {
+        MongoDBPersistence mongo;
+        public ExperimentController()
+        {
+            mongo = new MongoDBPersistence();
+        }
+
+
+        //POST: Home/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [Route("api/home/index")]
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            //return View(mongo.GetExperimentsByCrop("Raps")); Test
+            return View(mongo.GetExperiments());
         }
-        public IActionResult NIndex()
+
+        //[HttpGet] Doesnt work
+        //public ActionResult GetByYear(string year)
+        //{
+
+        //    return View("Index", mongo.GetExperimentsByYear(year));
+        //}
+        [Route("api/Home/Experiment")]
+        [HttpGet]
+        public ActionResult Create()
         {
-            return View();
+            return View(new Experiment());
         }
-        public IActionResult Create()
-        {
-            return View();
-        }
-        public IActionResult Edit()
-        {
-            return View();
-        }
-   
-        [Route("api/Home/CreateExperiment")]
-        [Produces("application/json")]
+        [Route("api/Home/Experiment")]
         [HttpPost]
-        public IActionResult CreateExperiment(bool[] toRoll)
+        public ActionResult Create(Experiment experiment)
         {
-            int toReturn = 3; 
-
-            return Ok(toReturn);
+            mongo.Save(experiment);
+            return RedirectToAction("Index");
         }
-        public IActionResult About()
+        [Route("api/Home/Edit")]
+        [HttpGet]
+        public ActionResult Edit(string id)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            List<Experiment> experiments = mongo.FindExperiment(id);
+            TempData["ObjectID"] = experiments[0].Id.ToString();
+            return View(experiments[0]);
         }
 
-        public IActionResult Error()
+        [Route("api/Home/Edit")]
+        [HttpPost]
+        public ActionResult Edit(Experiment experiment)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                string id = TempData["ObjectID"].ToString();
+                mongo.Edit(experiment, id);
+            }
+            return RedirectToAction("Index");
         }
+        [Route("api/Home/Delete")]
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            mongo.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
