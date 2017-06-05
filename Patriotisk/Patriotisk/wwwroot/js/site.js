09b9ccc118dfec74af2a7c48e4f6d6e1d76a44e2
Julie <sindhu_89@msn.com>
2017-06-05 10:52:52 +0200

    Working dynamic table.

--- Patriotisk/Patriotisk/wwwroot/js/site.js
+++ Patriotisk/Patriotisk/wwwroot/js/site.js
@@ -1,30 +1,30 @@
 ﻿$(document).ready(function () {
-    //$("#createTable").click(function () {
-    //    setupTable(); 
-    });
+  
 });
 
 function setupTable() {
     var rows = parseInt(document.getElementById("rows").value);
-    var columns = parseInt(document.getElementById("columns").value); 
+    var columns = parseInt(document.getElementById("columns").value);
 
     var tableDiv = document.getElementById("dynamicTable");
 
-    var table = document.createElement('TABLE');
-    table.border = '1';
+    var table = document.createElement("TABLE");
+    table.setAttribute("contenteditable", "true"); 
+    table.border = "1";
 
-    var tableBody = document.createElement('TBODY');
+    var tableBody = document.createElement("TBODY");
     table.appendChild(tableBody);
 
     for (var i = 0; i < rows; i++) {
-        var tr = document.createElement('TR');
-        tableBody.appendChild(tr);
+        var tableRow = document.createElement("TR");
+
+        tableBody.appendChild(tableRow);
 
         for (var j = 0; j < columns; j++) {
-            var td = document.createElement('TD');
-            td.width = '100';
-            td.appendChild(document.createTextNode("Cell " + i + "," + j));
-            tr.appendChild(td);
+            var tableColumn = document.createElement("TD");
+            tableColumn.width = "100";
+            tableColumn.appendChild(document.createTextNode(""));
+            tableRow.appendChild(tableColumn);
         }
     }
     tableDiv.appendChild(table);
