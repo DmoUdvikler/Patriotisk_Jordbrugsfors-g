b235ad116d7472695d7e80a1d09ab09b70b755b0
RuneMark <Ruras86@gmail.com>
2017-06-07 15:24:38 +0200

    Fiksede en heeeeeel masse!  - Ændrede tekst fra Engelsk til Dansk.  - Satte Julies pæne graf ind. Note: Hvis grafen ikke virker pga. Javascript, så slet cookies(Chrome:Ctrl+Shift+Delete(Slet cookies/cache))

--- Patriotisk/Patriotisk/wwwroot/js/site.js
+++ Patriotisk/Patriotisk/wwwroot/js/site.js
@@ -1,7 +1,39 @@
 ﻿$(document).ready(function () {
-  
+    createGraph();
 });
+function createGraph(/*numberArray*/) {
+    var numberArray = [[3347, "Ubehandlet 40 pl"], [3322, "Ubehandlet 40 pl"],
+    [3442, "1,5 l Folicur Xpert 40 pl"], [3488, "1,5 l Folicur Xpert 20 pl"]];
+    // var numberArray = [ [2,"Caryx"],[4,"Folicur Xpert"]]; 
+    var c = document.getElementById("graphCanvasID");
+    var context = c.getContext("2d");
+    var segmentHeight;
+    var margin = 4;
+    var numberOfSegments = numberArray.length;
+    var canvasHeight = 500;
+    var canvasWidth = 600;
+    var segmentWidth = (canvasWidth / numberOfSegments) - (margin * 2);
+    var ratio;
+    var highestNumber = 0;
+    var maxHeightOfSegment = canvasHeight - 40;
 
+    for (i = 0; i < numberOfSegments; i++) {
+        if (numberArray[i][0] > highestNumber)
+            highestNumber = numberArray[i][0];
+    }
+    for (i = 0; i < numberOfSegments; i++) {
+        ratio = numberArray[i][0] / highestNumber;
+        segmentHeight = ratio * maxHeightOfSegment;
+        context.fillStyle = "#47d147";
+        context.textAlign = "center";
+        context.fillRect(margin + ((i * canvasWidth) / numberOfSegments), canvasHeight - segmentHeight, segmentWidth, segmentHeight);
+        context.fillStyle = "#000000";
+        context.font = "12px Arial";
+        context.fillText(numberArray[i][0].toString(), (i * canvasWidth / numberOfSegments) + ((canvasWidth / numberOfSegments) / 2), canvasHeight - segmentHeight - 10);
+        context.font = "12px Arial";
+        context.fillText(numberArray[i][1].toString(), (i * canvasWidth / numberOfSegments) + ((canvasWidth / numberOfSegments) / 2), canvasHeight - 1);
+    }
+}
 function setupTable() {
     var rows = parseInt(document.getElementById("rows").value);
     var columns = parseInt(document.getElementById("columns").value);
