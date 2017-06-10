399787ffe50b841bc2261a8365624ea8a25f100f
Julie <sindhu_89@msn.com>
2017-06-10 15:54:37 +0200

    Worked with validation of email. The one on index page works. The one in seperate view does not.

--- Patriotisk/Patriotisk/Controllers/NewsController.cs
+++ Patriotisk/Patriotisk/Controllers/NewsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Patriotisk.Models;
 
 namespace Patriotisk.Controllers
 {
@@ -14,5 +15,26 @@ namespace Patriotisk.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Subscriber());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Subscriber Subscriber)
+        {
+            if (ModelState.IsValid)
+            {
+                //does something 
+            }
+            else
+            {
+              
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ Patriotisk/Patriotisk/Models/Subscriber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patriotisk.Models
+{
+    public class Subscriber
+    {
+        
+        [Display(Name = "Navn")]
+        [Required(ErrorMessage = "Navn er nødvendigt.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Emailaddresse")]
+        [Required(ErrorMessage = "Email er nødvendig.")]
+        [EmailAddress(ErrorMessage = "Ikke gyldig Email")]
+        public string Email { get; set; }
+    }
+}
